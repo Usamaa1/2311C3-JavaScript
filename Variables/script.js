b8659fa04ee72614888fae60543d5117d61b6fbf
usamaa1 <usamariz1997@gmail.com>
2024-03-05 13:09:09 +0500

    Input and Variables in JS

---
+++ Variables/script.js
@@ -0,0 +1,29 @@
+// Initializing
+// var a;
+// // Declare
+// a = 23;
+
+// var a = "usama";
+
+let a;
+a = 34;
+
+// let a = "usama";
+
+ a = "usama";
+
+ const pi = 3.14;
+
+//  pi = 4.3;
+
+c = 43;
+
+let name = prompt("Enter your Name");
+
+console.log(name)
+
+console.log(c)
+
+console.log(pi)
+
+console.log(a);
---
+++ showingOutput/script.js
@@ -0,0 +1,16 @@
+// alert("Hello World JavaScript!");
+
+// console.log("Hello Zahid");
+
+// document.write("Hello Farhan");
+
+
+// prompt("Enter your Name");
+
+
+
+
+
+
+
+
