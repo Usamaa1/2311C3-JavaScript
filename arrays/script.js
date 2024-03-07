aca3992b733286610834b608a8a1be710106f27b
usamaa1 <usamariz1997@gmail.com>
2024-03-07 12:32:07 +0500

    strings and arrays completed

---
+++ arrays/script.js
@@ -0,0 +1,21 @@
+let names = ['Zahid','Aslam','Iftikhar','Haris','Saqlain'];
+
+console.log(names)
+
+
+
+console.log(names[2])
+
+
+names[3] = 'Salman';
+
+
+console.log(names)
+
+
+// let -jksfdajkjdfsk =10;
+
+// let &jfkdsjfkj = 90
+// let 8934543jkfdjsaj
+
+
---
+++ string/script.js
@@ -0,0 +1,17 @@
+
+let subject1 =  prompt("Enter subject 1");
+let subject2 = prompt("Enter subject 2");
+
+
+console.log("I have enrolled in two subjects one of them is " + subject1 + " and another one is " + subject2 );
+
+console.log(subject1 +" "+ subject2)
+
+console.log(`I have enrolled in two subjects one of them is ${subject1} and another one is ${subject2}`);
+
+console.log(`${subject1} ${subject2}`)
+
+
+
+
+
