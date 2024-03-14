41f4d60be7c268bbc35f2dea1fdf590ce97ebd30
usamaa1 <usamariz1997@gmail.com>
2024-03-14 12:10:15 +0500

    coditional statements completed

--- conditional statement/script.js
+++ conditional statement/script.js
@@ -30,23 +30,61 @@
 // }
 
 
-let isAlive = prompt("Enter the condition");
+// let isAlive = prompt("Enter the condition");
 
 
-if(isAlive === "Yes")
+// if(isAlive === "Yes")
+// {
+//     console.log("You are applicable for NIC");
+// }
+// else if(isAlive === "No")
+// {
+//     console.log("You are applicable for death certificate")
+// }
+// else
+// {
+//     console.log("Kindly Enter valid Value");
+// }
+
+
+
+
+// let age = prompt("Enter your age");
+
+
+
+// if(age >= 18 && age <= 80)
+// {
+//     console.log("Congratulations 🎉🎉 You are applicable for driving License")
+// }
+// else if(age > 80)
+// {
+//     console.log("Sorry 😔😔 you can not drive a car in this age.")
+// }
+// else if(age < 18)
+// {
+//     console.log("Sorry 😔😔 you are a child Just focus on your study 👨‍🎓👨‍🏫")
+// }
+// else
+// {
+//     console.log("Kindly Enter valid Age")
+// }
+
+
+// !true = false;
+
+
+
+let isAlive = true;
+
+if(!isAlive)
 {
-    console.log("You are applicable for NIC");
-}
-else if(isAlive === "No")
-{
-    console.log("You are applicable for death certificate")
+    console.log("You are applicable for achieving Death Certificate! ✍");
 }
 else
 {
-    console.log("Kindly Enter valid Value");
+    console.log("You are not dead");
 }
-
-
 
 
 
