3d504075e2db5bc645f9e0477b93500e6fe48e64
usamaa1 <usamariz1997@gmail.com>
2024-03-26 11:49:54 +0500

    Functions completed

---
+++ functions/script.js
@@ -0,0 +1,101 @@
+// let a = 10;
+// let  b = 20;
+// console.log(a + b)
+
+
+// let c = 20;
+// let d = 40;
+// console.log(c + d)
+
+
+// function sum() {
+//     console.log(a + b)
+// }
+
+// let a = 10
+// let b = 20;
+
+// sum()
+
+// a = 20;
+// b = 60
+
+// sum()
+
+// function sum(a,b)
+// {
+//     // console.log(a + b)
+//     return a + b;
+// }
+
+
+// // sum(34,45)
+
+// console.log(sum(34,45) - 56)
+
+// let a = 10;
+// let a = 20;
+
+// function subtract()
+// {
+//     let a = prompt("Enter first number")
+//     let b = prompt("Enter second number")
+
+//     console.log(a - b)
+// }
+
+
+// subtract();
+
+// console.log("This is from function",a)
+
+
+
+// let a = 10;
+
+// function funScope()
+// {
+//     console.log("This variable is from function",a);
+//     a = 50;
+// }
+
+// funScope();
+// console.log(a)
+
+
+// let sum = function(a,b)
+// {
+//     console.log(a + b)
+// }
+
+
+// sum(30,40)
+
+
+// let sum = (a,b) => {
+//     console.log("This is the result of sum:")
+//     console.log(a + b)
+// }
+
+
+// sum(1,2)
+
+
+// let square = a => a**2;
+
+// console.log(square(3))
+
+
+// function square(a) { return a**2};
+
+// console.log(square(2))
+
+
+
+
+
+
+
+
+
+
