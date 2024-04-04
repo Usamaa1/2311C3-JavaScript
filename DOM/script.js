249012cdbf432713596e5841819d3c606398c123
usamaa1 <usamariz1997@gmail.com>
2024-04-04 11:54:53 +0500

    DOM starts

---
+++ DOM/script.js
@@ -0,0 +1,44 @@
+// let para = document.getElementById('p1');
+// let heading = document.getElementsByTagName('h1');
+// let heading = document.getElementsByClassName('h1');
+
+
+
+
+// heading[0].innerText = "hello heading"
+
+// let heading = document.querySelector('.h1')
+
+// heading.innerHTML = "Hello World <a href='#'>for more</a>"
+
+
+// console.log(heading)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Objects/script.js
@@ -0,0 +1,105 @@
+// const persons = {
+//     name: "Javed",
+//     age: 12,
+//     class: 8,
+//     address: "House no. 67, street 12, Karachi,Pakistan",
+//     colors: ["Blue","Purple","Yellow"],
+//     account:{
+//         accountHolderName: "Javed Amjad Khan",
+//         pass: 234,
+//         accountType: "Kids Monthly Saving Account",
+//         balance: 1200
+//     }
+// }
+
+// console.log(persons)
+
+// console.log(persons.name);
+// console.log(persons.colors[2])
+// console.log(persons.account.balance)
+
+
+// const persons = [
+//     {
+//         firstName: "Zahid",
+//         lastName: "Hanif",
+//         email: "zahidHanif@gmail.com",
+//         pass: 234
+//     },
+//     {
+//         firstName: "Jamshed",
+//         lastName: "Ghafoor",
+//         email: "jamshed@gmail.com",
+//         pass: 123
+//     },
+//     {
+//         firstName: "Hina",
+//         lastName: "Khan",
+//         email: "hina@gmail.com",
+//         pass: 678
+//     }
+// ]
+
+// console.log(persons)
+
+// const arr = [1,2,3,4,5]
+
+// console.log(arr[1])
+
+// for(let i =0; i < persons.length; i++)
+// {
+//     console.log(persons[i].firstName)
+// }
+
+
+
+
+// const levi = {
+//     firstName: "Ammar",
+//     lastName: "Ackerman",
+//     birthYear: 1996,
+//     age: function(currentYear)
+//     { 
+//         console.log(currentYear - this.birthYear)
+
+//     }
+
+// }
+// const mikasa = {
+//     firstName: "Mikasa",
+//     lastName: "Ackerman",
+//     birthYear: 2000,
+   
+
+// }
+
+// mikasa.age = levi.age;
+
+
+// levi.age(2024)
+
+// mikasa.age(2024)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
